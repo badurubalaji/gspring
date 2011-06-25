d43df6dbe6b20cb78afc46c2789cf34e2ad53085
buzmakoff@gmail.com <buzmakoff@gmail.com@dfbe2eaa-e7e8-8553-ce93-d2b8a4e47333>
2011-06-25 16:20:15 +0000

    Removed unneeded code

--- framework/src/main/java/org/gspring/mvc/GwtHandlerMapping.java
+++ framework/src/main/java/org/gspring/mvc/GwtHandlerMapping.java
@@ -1,22 +1,27 @@
 package org.gspring.mvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.BeanNotOfRequiredTypeException;
 import org.springframework.core.Ordered;
 import org.springframework.util.Assert;
-import org.springframework.web.servlet.HandlerExecutionChain;
 import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.handler.AbstractDetectingUrlHandlerMapping;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 /**
  * MVC Handler Mapping for GWT RPC calls
  * 
  * @author a.buzmakoff
- *
+ * 
  */
-class GwtHandlerMapping implements HandlerMapping, Ordered, GwtServiceRegistryAware {
+class GwtHandlerMapping extends AbstractDetectingUrlHandlerMapping implements HandlerMapping, Ordered {
 	private static final String GWT_DEFAULT_EXT = ".gwt";
-
-	private GwtServiceRegistry serviceRegistry;
 
 	public String extension = GWT_DEFAULT_EXT;
 
@@ -27,28 +32,54 @@ class GwtHandlerMapping implements HandlerMapping, Ordered, GwtServiceRegistryAw
 	}
 
 	@Override
-	public int getOrder() {
-		return Ordered.HIGHEST_PRECEDENCE;
-	}
+	protected String[] determineUrlsForHandler(String beanName) {
+		try {
+			RemoteService bean = getApplicationContext().getBean(beanName, RemoteService.class);
 
-	@Override
-	public void setServiceRegistry(GwtServiceRegistry serviceRegistry) {
-		this.serviceRegistry = serviceRegistry;
-	}
+			checkInheritsRemoteServiceDirectly(bean);
 
-	@Override
-	public HandlerExecutionChain getHandler(HttpServletRequest request) throws Exception {
-		String requestURI = request.getRequestURI();
+			List<String> relativePaths = new ArrayList<String>();
 
-		if (!requestURI.endsWith(extension)) {
+			findRelativePaths(bean.getClass(), relativePaths);
+
+			return relativePaths.toArray(new String[0]);
+		} catch (BeanNotOfRequiredTypeException e) {
 			return null;
 		}
-
-		Object delegate = serviceRegistry.retrieveDelegate(requestURI);
-
-		Assert.notNull(delegate, "No such URI registered");
-
-		return new HandlerExecutionChain(delegate);
 	}
 
+	private void findRelativePaths(Class<?> beanClass, List<String> relativePaths) {
+		for (Class<?> interf : beanClass.getInterfaces()) {
+			if (isRemoteServiceType(interf)) {
+				RemoteServiceRelativePath relativePathAnnotation = beanClass.getAnnotation(RemoteServiceRelativePath.class);
+				if (relativePathAnnotation != null) {
+					relativePaths.add(relativePathAnnotation.value());
+				} else {
+					logger.warn("Bean of type [" + beanClass + "] does not have annotation of type [" + RemoteServiceRelativePath.class + "] and is skipped");
+				}
+			} else {
+				findRelativePaths(interf, relativePaths);
+			}
+		}
+	}
+
+	private void checkInheritsRemoteServiceDirectly(Object bean) {
+		for (Class<?> interfce : bean.getClass().getInterfaces()) {
+			Assert.isTrue(!isRemoteServiceType(interfce), "Bean [" + (Class<?>) bean.getClass() + "] inherits [" + RemoteService.class + "] directly");
+		}
+	}
+
+	private boolean isRemoteServiceType(Class<?> interf) {
+		return RemoteService.class.equals(interf);
+	}
+
+	@Override
+	protected Object lookupHandler(String urlPath, HttpServletRequest request) throws Exception {
+		for (String relativePath : getHandlerMap().keySet()) {
+			if (urlPath.endsWith(relativePath)) {
+				return getHandlerMap().get(relativePath);
+			}
+		}
+		return null;
+	}
 }
