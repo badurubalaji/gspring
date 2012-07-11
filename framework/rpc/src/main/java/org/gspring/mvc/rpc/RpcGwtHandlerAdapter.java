dd6f71453a8795980de476d6ba7694945e0fc752
buzmakoff@gmail.com <buzmakoff@gmail.com@dfbe2eaa-e7e8-8553-ce93-d2b8a4e47333>
2012-07-11 11:24:44 +0000

    Reduced objects creating

---
+++ framework/rpc/src/main/java/org/gspring/mvc/rpc/RpcGwtHandlerAdapter.java
@@ -0,0 +1,59 @@
+package org.gspring.mvc.rpc;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.ServletConfigAware;
+import org.springframework.web.context.ServletContextAware;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * MVC Handler Adapter for GWT RPC calls
+ *
+ * @author a.buzmakoff
+ */
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+class RpcGwtHandlerAdapter implements HandlerAdapter, ServletContextAware, ServletConfigAware {
+    private static final int NEVER_LAST_MODIFIED = -1;
+
+    private ServletContext servletContext;
+    private ServletConfig servletConfig;
+
+    @Resource
+    private View gwtRpcView;
+
+    @Override
+    public boolean supports(Object handler) {
+        return handler instanceof RemoteService;
+    }
+
+    @Override
+    public ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return new RpcGwtModelAndView(gwtRpcView, handler);
+    }
+
+    @Override
+    public long getLastModified(HttpServletRequest request, Object handler) {
+        return NEVER_LAST_MODIFIED;
+    }
+
+    @Override
+    public void setServletContext(ServletContext servletContext) {
+        this.servletContext = servletContext;
+    }
+
+    @Override
+    public void setServletConfig(ServletConfig servletConfig) {
+        this.servletConfig = servletConfig;
+    }
+}
---
+++ framework/rpc/src/main/java/org/gspring/mvc/rpc/RpcGwtHandlerMapping.java
@@ -0,0 +1,98 @@
+package org.gspring.mvc.rpc;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
+import org.springframework.cache.Cache.ValueWrapper;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.support.NoOpCacheManager;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+import org.springframework.web.servlet.handler.AbstractDetectingUrlHandlerMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * MVC Handler Mapping for GWT RPC calls
+ *
+ * @author a.buzmakoff
+ */
+@Component
+class RpcGwtHandlerMapping extends AbstractDetectingUrlHandlerMapping {
+    @Autowired(required = false)
+    private CacheManager cacheManager = new NoOpCacheManager();
+
+    @Override
+    protected String[] determineUrlsForHandler(String beanName) {
+        BeanFactory context = getApplicationContext();
+
+        if (!context.isTypeMatch(beanName, RemoteService.class)) {
+            return null;
+        }
+
+        RemoteService bean = context.getBean(beanName, RemoteService.class);
+
+        checkInheritsRemoteServiceDirectly(bean);
+
+        List<String> relativePaths = new ArrayList<String>();
+
+        findRelativePaths(bean.getClass(), relativePaths);
+
+        return relativePaths.toArray(new String[0]);
+
+    }
+
+    private void findRelativePaths(Class<?> beanClass, List<String> relativePaths) {
+        for (Class<?> interf : beanClass.getInterfaces()) {
+            if (isRemoteServiceType(interf)) {
+                RemoteServiceRelativePath relativePathAnnotation = beanClass.getAnnotation(RemoteServiceRelativePath.class);
+                if (relativePathAnnotation != null) {
+                    relativePaths.add(relativePathAnnotation.value());
+                } else {
+                    logger.warn("Bean of type [" + beanClass + "] does not have annotation of type [" + RemoteServiceRelativePath.class + "] and is skipped");
+                }
+            } else {
+                findRelativePaths(interf, relativePaths);
+            }
+        }
+    }
+
+    private void checkInheritsRemoteServiceDirectly(Object bean) {
+        for (Class<?> interfce : bean.getClass().getInterfaces()) {
+            Assert.isTrue(!isRemoteServiceType(interfce), "Bean [" + (Class<?>) bean.getClass() + "] inherits [" + RemoteService.class + "] directly");
+        }
+    }
+
+    private boolean isRemoteServiceType(Class<?> interf) {
+        return RemoteService.class.equals(interf);
+    }
+
+    @Override
+    protected Object lookupHandler(String urlPath, HttpServletRequest request) throws Exception {
+        ValueWrapper cached = getCache().get(urlPath);
+        if (cached != null) {
+            return cached.get();
+        }
+
+        for (String relativePath : getHandlerMap().keySet()) {
+            if (urlPath.endsWith(relativePath)) {
+                Object value = getHandlerMap().get(relativePath);
+
+                getCache().put(urlPath, value);
+
+                return value;
+            }
+        }
+
+        return null;
+    }
+
+    private Cache getCache() {
+        final String name = getClass().getCanonicalName();
+        return cacheManager.getCache(name);
+    }
+}
