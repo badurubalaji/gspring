bfddcacf0473ef6177ab14399e5b402424e114a8
buzmakoff@gmail.com <buzmakoff@gmail.com@dfbe2eaa-e7e8-8553-ce93-d2b8a4e47333>
2014-01-18 17:23:54 +0000

    Added the ability transform server side exceptions (mostly inherited from frameworks) to the client user defined

---
+++ framework/src/main/java/org/gspring/mvc/rpc/ExceptionsTransformer.java
@@ -0,0 +1,14 @@
+package org.gspring.mvc.rpc;
+
+/**
+ * Transformer between server side exceptions and client side aware exceptions.
+ *
+ * Example for:
+ * org.springframework.security.access.AccessDeniedException
+ * it could rethrow GWT client aware exceptions
+ *
+ * @author a.buzmakoff
+ */
+public interface ExceptionsTransformer {
+  RuntimeException transform(Exception serverSideError);
+}
