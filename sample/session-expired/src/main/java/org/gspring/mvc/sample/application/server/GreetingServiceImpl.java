208c3c819379459f9f4f8693419e3f9efcee18dd
buzmakoff@gmail.com <buzmakoff@gmail.com@dfbe2eaa-e7e8-8553-ce93-d2b8a4e47333>
2014-07-01 20:49:48 +0000

    Fixed compilation problems

--- sample/session-expired/src/main/java/org/gspring/mvc/sample/application/server/GreetingServiceImpl.java
+++ sample/session-expired/src/main/java/org/gspring/mvc/sample/application/server/GreetingServiceImpl.java
@@ -1,7 +1,7 @@
 package org.gspring.mvc.sample.application.server;
 
 import org.gspring.mvc.sample.application.client.GreetingService;
-import org.gspring.mvc.sample.application.shared.security.ApplicationException;
+import org.gspring.mvc.sample.application.shared.security.AppSecurityException;
 import org.springframework.stereotype.Service;
 
 import java.text.MessageFormat;
@@ -9,7 +9,7 @@ import java.text.MessageFormat;
 @Service("greetingService")
 public class GreetingServiceImpl implements GreetingService {
 	@Override
-	public String greetServer(String name) throws ApplicationException {
+	public String greetServer(String name) throws AppSecurityException {
 		return MessageFormat.format("Hello, {0}!", name);
 	}
 }
